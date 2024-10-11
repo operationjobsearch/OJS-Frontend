1261df03fdab21534cf608d512049d865cb08b94
Nicole Freites <nicolejfreites@gmail.com>
2024-10-11 12:30:30 -0400

    (wip) Created room with shapes within
    Room contains four walls and a floor with three cube bases and different shapes on top. Used to practice positioning objects within the scene.

--- ojs-frontend-react/src/App.tsx
+++ ojs-frontend-react/src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+/* Constructors for shapes being used in scene  */
+/* Creates cube shape using boxGeometry */
+const Cube = ({position, size, color}: {position: [number, number, number]; size: [number, number, number]; color: string}) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <mesh position={position}>
+    <boxGeometry args={size} />
+    <meshStandardMaterial color={color} />
+  </mesh>
   )
 }
 
-export default App
+/* Creates cone shape using coneGeometry */
+const Cone = ({position, size, color}: {position: [number, number, number]; size: [number, number, number]; color: string}) => {
+  return (
+  <mesh position={position}>
+    <coneGeometry args={size} />
+    <meshStandardMaterial color={color} />
+  </mesh>
+  )
+}
+
+/** 
+ * Creates sphere shape using sphereGeometry. 
+ * Also detects pointer coming in contact with sphere mesh.
+ * Changes color of shape when pointer enters mesh.
+*/
+const Sphere = ({position, size}: {position: [number, number, number]; size: [number, number, number]; color: string}) => {
+  const ref = useRef();
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  useFrame((state , delta) => {
+    ref.current.rotation.y += delta * 0.2;
+  })
+
+  /* stopPropagation ensures mouse enters the mesh and event is only contained to the mesh */
+  return (
+    <mesh 
+      position={position} 
+      ref={ref} 
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={() => setIsHovered(false)}
+    >
+      <sphereGeometry args={size}/>
+      <meshStandardMaterial 
+        color={isHovered ? "red" : "pink"} 
+        wireframe
+      />
+    </mesh>
+  )
+}
+
+/* Creates ring shape using torusGeometry */
+const Torus = ({position, size, color}: {position: [number, number, number]; size: [number, number, number, number]; color: string}) => {
+  return (
+    <mesh position={position}>
+      <torusGeometry args={size}/>
+      <meshStandardMaterial color={color}/>
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+      <Canvas>  
+        {/* ambientLight is where lighting shines on all objects in scene equally. */}
+        <ambientLight intensity={1}/>
+
+        {/* Room Walls and Floor */}
+        <group>
+          <Cube position={[0,0,5]} size={[20,5,0.5]} color={"green"} />
+          <Cube position={[10.5,0,-5]} size={[1,5,-20]} color={"green"} />
+          <Cube position={[-10.5,0,-5]} size={[1,5,-20]} color={"green"} />
+          <Cube position={[0,0,-15]} size={[20,5,0.5]} color={"green"} />
+          <Cube position={[0,-2.5,-5]} size={[20,0,20]} color={"lightblue"} />
+        </group>
+  
+        {/** 
+         * Shapes Within Room: 
+         * 
+         * Cube base with different shape on top
+         */}
+        <Cube position={[0,-2,-5]} size={[2,2,2]} color={"gray"} />
+        <Sphere position={[0, 0, -5]} size={[1, 30, 30]} color={"pink"}/>
+  
+        <Cube position={[9,-2,-14]} size={[2,2,2]} color={"gray"} />
+        <Cone position={[9, 0, -14]} size={[1, 2, 4]} color={"orange"}/>
+  
+        <Cube position={[-9,-2,-14]} size={[2,2,2]} color={"gray"} />
+        <Torus position={[-9, 0, -14]} size={[0.9, 0.1, 30, 30]} color={"purple"}/>
+      </Canvas>
+  );
+  }
